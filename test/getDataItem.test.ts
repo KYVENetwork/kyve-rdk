b49f917297d5f3792dc225a4e7b8ac279f565766
Troy Kessler <troy.kessler99@gmail.com>
2022-11-11 17:04:00 +0100

    added more tests to rdk

--- test/getDataItem.test.ts
+++ test/getDataItem.test.ts
@@ -21,7 +21,7 @@ describe('runtime tests', () => {
     core = new Node(runtime);
   });
 
-  test('getDataItem: collect data item of start key', async () => {
+  test('collect data item of start key', async () => {
     // ASSERT
     const getDataItemSpy = jest.spyOn(runtime, 'getDataItem');
 
@@ -35,23 +35,25 @@ describe('runtime tests', () => {
     expect(getDataItemSpy).not.toThrow();
   });
 
-  test('nextKey: get the next 10 keys', async () => {
+  test('get the next keys for first data bundle', async () => {
     // ASSERT
     const nextKeySpy = jest.spyOn(runtime, 'nextKey');
 
     let currentKey = poolConfig.start_key;
 
     // ACT
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < parseInt(poolConfig.max_bundle_size); i++) {
       currentKey = await runtime.nextKey(currentKey);
     }
 
     // ASSERT
-    expect(nextKeySpy).toHaveBeenCalledTimes(10);
+    expect(nextKeySpy).toHaveBeenCalledTimes(
+      parseInt(poolConfig.max_bundle_size)
+    );
     expect(nextKeySpy).not.toThrow();
   });
 
-  test('getDataItem: collect first 10 data items', async () => {
+  test('get the next data items for first data bundle', async () => {
     // ASSERT
     const getDataItemSpy = jest.spyOn(runtime, 'getDataItem');
     const nextKeySpy = jest.spyOn(runtime, 'nextKey');
@@ -59,7 +61,7 @@ describe('runtime tests', () => {
     let currentKey = poolConfig.start_key;
 
     // ACT
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < parseInt(poolConfig.max_bundle_size); i++) {
       for (let source of runtimeConfig.sources) {
         await runtime.getDataItem(core, source, currentKey);
         currentKey = await runtime.nextKey(currentKey);
@@ -67,10 +69,92 @@ describe('runtime tests', () => {
     }
 
     // ASSERT
-    expect(getDataItemSpy).toHaveBeenCalledTimes(10);
+    expect(getDataItemSpy).toHaveBeenCalledTimes(
+      parseInt(poolConfig.max_bundle_size)
+    );
     expect(getDataItemSpy).not.toThrow();
 
-    expect(nextKeySpy).toHaveBeenCalledTimes(10);
+    expect(nextKeySpy).toHaveBeenCalledTimes(
+      parseInt(poolConfig.max_bundle_size)
+    );
     expect(nextKeySpy).not.toThrow();
+  });
+
+  test('get the next data items for first data bundle including transformation', async () => {
+    // ASSERT
+    const getDataItemSpy = jest.spyOn(runtime, 'getDataItem');
+    const transformDataItemSpy = jest.spyOn(runtime, 'transformDataItem');
+    const nextKeySpy = jest.spyOn(runtime, 'nextKey');
+
+    let currentKey = poolConfig.start_key;
+
+    // ACT
+    for (let i = 0; i < parseInt(poolConfig.max_bundle_size); i++) {
+      for (let source of runtimeConfig.sources) {
+        const dataItem = await runtime.getDataItem(core, source, currentKey);
+        await runtime.transformDataItem(dataItem);
+        currentKey = await runtime.nextKey(currentKey);
+      }
+    }
+
+    // ASSERT
+    expect(getDataItemSpy).toHaveBeenCalledTimes(
+      parseInt(poolConfig.max_bundle_size)
+    );
+    expect(getDataItemSpy).not.toThrow();
+
+    expect(transformDataItemSpy).toHaveBeenCalledTimes(
+      parseInt(poolConfig.max_bundle_size)
+    );
+    expect(transformDataItemSpy).not.toThrow();
+
+    expect(nextKeySpy).toHaveBeenCalledTimes(
+      parseInt(poolConfig.max_bundle_size)
+    );
+    expect(nextKeySpy).not.toThrow();
+  });
+
+  test('create bundle summary for first data bundle', async () => {
+    // ASSERT
+    const getDataItemSpy = jest.spyOn(runtime, 'getDataItem');
+    const transformDataItemSpy = jest.spyOn(runtime, 'transformDataItem');
+    const nextKeySpy = jest.spyOn(runtime, 'nextKey');
+    const summarizeDataBundleSpy = jest.spyOn(runtime, 'summarizeDataBundle');
+
+    let currentKey = poolConfig.start_key;
+    let bundle = [];
+
+    // ACT
+    for (let i = 0; i < parseInt(poolConfig.max_bundle_size); i++) {
+      for (let source of runtimeConfig.sources) {
+        const dataItem = await runtime.getDataItem(core, source, currentKey);
+        const transformedDataItem = await runtime.transformDataItem(dataItem);
+
+        bundle.push(transformedDataItem);
+
+        currentKey = await runtime.nextKey(currentKey);
+      }
+    }
+
+    await runtime.summarizeDataBundle(bundle);
+
+    // ASSERT
+    expect(getDataItemSpy).toHaveBeenCalledTimes(
+      parseInt(poolConfig.max_bundle_size)
+    );
+    expect(getDataItemSpy).not.toThrow();
+
+    expect(transformDataItemSpy).toHaveBeenCalledTimes(
+      parseInt(poolConfig.max_bundle_size)
+    );
+    expect(transformDataItemSpy).not.toThrow();
+
+    expect(nextKeySpy).toHaveBeenCalledTimes(
+      parseInt(poolConfig.max_bundle_size)
+    );
+    expect(nextKeySpy).not.toThrow();
+
+    expect(summarizeDataBundleSpy).toHaveBeenCalledTimes(1);
+    expect(summarizeDataBundleSpy).not.toThrow();
   });
 });
